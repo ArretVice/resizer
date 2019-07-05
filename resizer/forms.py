9eb2b0266f33334788814c263d64df2e82e12476
ArretVice <arretvice@gmail.com>
2019-07-05 16:50:58 +0300

    Added main functionality for resizing images

--- resizer/forms.py
+++ resizer/forms.py
@@ -2,7 +2,17 @@ from django import forms
 from .models import UploadedImage
 
 
+VALID_FORMATS = ['jpeg', 'jpg', 'png']
+
 class ImageUploadForm(forms.ModelForm):
+    # checking for valid image formats
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        image_format = image.name.split('.')[-1].lower()
+        if image_format not in VALID_FORMATS:
+            raise forms.ValidationError(f'Error: the image must be in one of the following formats: {", ".join(VALID_FORMATS)}.')
+        return image
+
     class Meta:
         model = UploadedImage
         fields = ('image', )
--- resizer/views.py
+++ resizer/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.http import HttpResponse
 from .forms import ImageUploadForm
 from .models import UploadedImage
+from .functions import resize_image
 
+
+IMAGE_DIMENSIONS = {
+    'height': {
+        'min': 1,
+        'max': 9999,
+    },
+    'width': {
+        'min': 1,
+        'max': 9999,
+    },
+}
+HEIGHT_RANGE = range(IMAGE_DIMENSIONS['height']['min'], IMAGE_DIMENSIONS['height']['max'] + 1)
+WIDTH_RANGE = range(IMAGE_DIMENSIONS['width']['min'], IMAGE_DIMENSIONS['width']['max'] + 1)
 
 # Create your views here.
 def home(request):
     return render(request, 'resizer/home.html')
 
 def upload(request):
+    # add check for file format
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
@@ -18,12 +35,21 @@ def upload(request):
     return render(request, 'resizer/upload.html', {'form': form})
 
 def resize(request, image_id):
-    ### continue from here
     if request.method == 'POST':
-        form = ImageUploadForm(request.POST, request.FILES)#
-        if form.is_valid():#
-            image = form.save()#
-            return redirect('resizer:resize', image_id=image.pk)#
+        width = int(request.POST.get('width', None))
+        height = int(request.POST.get('height', None))
+        if (width in WIDTH_RANGE) and (height in HEIGHT_RANGE):
+            image = UploadedImage.objects.get(pk=image_id).image
+            # preserve image format by default
+            image_extension = image.name.split('.')[-1].lower()
+            if image_extension == 'jpg':
+                image_extension = 'jpeg'
+            resized_image = resize_image(image, width, height)
+            response = HttpResponse(content_type=f"image/{image_extension}")
+            resized_image.save(response, f'{image_extension.upper()}')
+            return response
+        else:
+            messages.error(request, 'Error: image dimensions out of range.')
     image = UploadedImage.objects.get(pk=image_id)
     return render(request, 'resizer/resize.html', {'image': image})
 
